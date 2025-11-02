14950217f0ff7f7b6ce8666fd07478830c4d79b0
Motya-s <matpro.ru2@gmail.com>
2025-11-02 17:04:07 +0300

    Add files via upload

---
+++ Practice 9 Задание 1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+class Person
+{
+    
+    private string name;
+    private int age;
+
+    public string Name
+    {
+
+        get { return name; }
+        set { name = value; }
+
+    }
+
+    public int Age
+    {
+
+        get { return age; }
+        set
+
+        {
+
+            if (value >= 0 && value <= 120)
+                age = value;
+            else
+                Console.WriteLine("Ошибка: возраст должен быть от 0 до 120.");
+
+        }
+
+    }
+    public Person(string name, int age)
+
+    {
+
+        Name = name;
+        Age = age;
+
+    }
+
+    public void SayHello()
+    {
+
+        Console.WriteLine($"Привет, я {Name}, мне {Age} лет!");
+
+    }
+}
+
+class Program
+{
+
+    static void Main()
+
+    {
+
+        Person p = new Person("Матвей", 17);
+        p.SayHello();
+        Console.WriteLine();
+
+    }
+
+}
