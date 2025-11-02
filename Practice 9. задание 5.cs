39bfa1f1a1f0b4849f8dc0482a48b28ad1280516
Motya-s <matpro.ru2@gmail.com>
2025-11-02 17:30:20 +0300

    Add files via upload

---
+++ Practice 9. задание 5.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+class Pet
+{
+
+    private string name;
+    private int energy;
+
+    public string Name
+
+    {
+
+        get { return name; }
+        set { name = value; }
+
+    }
+
+    public int Energy
+
+    {
+
+        get { return energy; }
+        set
+
+        {
+
+            if (value < 0)
+                energy = 0;
+            else if (value > 100)
+                energy = 100;
+            else
+                energy = value;
+
+        }
+
+    }
+
+    public Pet(string name, int energy)
+    {
+
+        Name = name;
+        Energy = energy;
+
+    }
+
+    public void Play()
+
+    {
+        Energy -= 20;
+        Console.WriteLine($"{Name} играет, энергия: {Energy}%.");
+
+    }
+
+    public void Rest()
+
+    {
+
+        Energy += 30;
+        Console.WriteLine($"{Name} отдыхает, энергия: {Energy}%.");
+
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Pet pet = new Pet("Шарик", 80);
+        pet.Play();
+        pet.Rest();
+
+        Pet pet2 = new Pet("Муська", 150);
+        pet2.Play();
+
+    }
+}
